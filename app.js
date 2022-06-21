3051e629de4dc706181a90aad9afcff7b0ebf71f
J. Christian Romero <jcromerohdz@gmail.com>
2022-06-21 15:55:52 -0700

    Argunment Pasing with Yargs

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 import chalk from 'chalk'
+import yargs from 'yargs'
+import {hideBin} from 'yargs/helpers'
 import getNotes from './notes.js'
 
-
-const msg = getNotes()
-console.log(msg)
-
-
-
-console.log(chalk.red.bold.inverse('Error!!'))
+const argv = yargs(process.argv.slice(2)) 
+	.command({
+		command: 'add',
+		desc: 'Create a Note',
+		builder:{
+			title: {
+				desc: 'Note Title',
+				demandOption: true,
+				type: 'string'
+			},
+			body: {
+				desc: 'Note Body',
+				demandOption: true,
+				type: 'string'
+			}
+		},
+		handler: (argv) => {
+    	console.log(`Title: ${argv.title},\nBody: ${argv.body}`)
+		}
+	})
+	.command('remove', 'Delete a Note', () => {}, (argv) => {
+    console.log('Deleting a Note')
+  })
+	.command('list', 'List Of Notes', () => {}, (argv) => {
+    console.log('List of notes')
+  })
+	.command('read', 'Read a Note', () => {}, (argv) => {
+    console.log('Reading a Note')
+  })
+	.version('1.0.0')
+  .argv
